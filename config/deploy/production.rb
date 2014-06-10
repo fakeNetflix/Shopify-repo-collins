d6516314e3f484162a8fa5ba8937b04e57604cfd
Dale Hamel <dale.hamel@srvthe.net>
2014-06-09 21:50:11 -0400

    Switched up some users/passwords to be more generic

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,13 +12,6 @@
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'util11.chi', 
-  user: 'deploy', 
-  roles: %w{app},
-  ssh_options: {
-    user: 'daleh',
-    forward_agent: true
-  }
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
--- scripts/populate.sh
+++ scripts/populate.sh
@@ -26,10 +26,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RESOURCE_DIR="${DIR}/../test/resources/"
 
 for i in `seq 300 310`; do
-  TAG="tumblrtag${i}"
+  TAG="SPM-000000${i}"
   LSHW_FILE="${RESOURCE_DIR}/lshw-${lshw_files[$((RANDOM%num_lshw_files))]}.xml"
   LLDP_FILE="${RESOURCE_DIR}/lldpctl-${lldp_files[$((RANDOM%num_lldp_files))]}.xml"
-  curl --basic -X PUT -H "Accept: text/plain" -u blake:admin:first "${URL}/${TAG}"
-  curl --basic -H "Accept: text/plain" -u blake:admin:first --data-urlencode "lldp@${LLDP_FILE}" --data-urlencode "lshw@${LSHW_FILE}" --data-urlencode 'CHASSIS_TAG=Testing this' "${URL}/${TAG}"
+  curl --basic -X PUT -H "Accept: text/plain" -u collins:collins "${URL}/${TAG}"
+  curl --basic -H "Accept: text/plain" -u collins:collins --data-urlencode "lldp@${LLDP_FILE}" --data-urlencode "lshw@${LSHW_FILE}" --data-urlencode 'CHASSIS_TAG=Testing this' "${URL}/${TAG}"
   sleep 10
 done
